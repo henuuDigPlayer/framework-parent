c9d1ccc17edbdd56186abc3f4a0b3f5a4f5011f3
2118089226@qq.com <2118089226@qq.com>
2018-12-17 15:28:35 +0800

    代码修改

--- framework-common/src/main/java/com/zjdex/framework/util/HttpRequestUtil.java
+++ framework-common/src/main/java/com/zjdex/framework/util/HttpRequestUtil.java
@@ -6,9 +6,12 @@ import org.jdom2.input.SAXBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -130,6 +133,49 @@ public class HttpRequestUtil {
         }
         return result.toString();
     }
+
+    /**
+     * 获取当前网络ip
+     * @param request
+     * @return
+     */
+    public static String getIpAddr(HttpServletRequest request){
+        String ipAddress = request.getHeader("x-forwarded-for");
+        Integer ipAddressLength = 15;
+        String division = ",";
+        String unknown = "unknown";
+
+        String localhost = "127.0.0.1";
+
+        String address = "0:0:0:0:0:0:0:1";
+        if(ipAddress == null || ipAddress.length() == 0 || unknown.equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+        if(ipAddress == null || ipAddress.length() == 0 || unknown.equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if(ipAddress == null || ipAddress.length() == 0 || unknown.equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+            if(ipAddress.equals(localhost) || ipAddress.equals(address)){
+                //根据网卡取本机配置的IP
+                InetAddress inet=null;
+                try {
+                    inet = InetAddress.getLocalHost();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+                ipAddress= inet.getHostAddress();
+            }
+        }
+        // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+        if(ipAddress!=null && ipAddress.length()> ipAddressLength){
+            if(ipAddress.indexOf(division)>0){
+                ipAddress = ipAddress.substring(0,ipAddress.indexOf(division));
+            }
+        }
+        return ipAddress;
+    }
+
 
     private static String xml2JSON(byte[] xml) {
         InputStream is = new ByteArrayInputStream(xml);
