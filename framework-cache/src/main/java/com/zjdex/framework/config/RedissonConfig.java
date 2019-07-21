2caf04c6d78a2b3ac5f15df5c32c246fc1e7c3ad
2118089226@qq.com <2118089226@qq.com>
2019-07-21 13:59:08 +0800

    1.4初始化

--- framework-cache/src/main/java/com/zjdex/framework/config/RedissonConfig.java
+++ framework-cache/src/main/java/com/zjdex/framework/config/RedissonConfig.java
@@ -3,19 +3,12 @@ package com.zjdex.framework.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.redisson.config.SingleServerConfig;
-import org.redisson.spring.cache.CacheConfig;
-import org.redisson.spring.cache.RedissonSpringCacheManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author lindj
