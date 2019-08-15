2173c46773597ee33e08cb963be484ac9bee342d
2118089226@qq.com <2118089226@qq.com>
2019-08-15 15:52:48 +0800

    修改

--- framework-common/src/main/java/com/zjdex/framework/util/http/HttpRequestUtil.java
+++ framework-common/src/main/java/com/zjdex/framework/util/http/HttpRequestUtil.java
@@ -1,12 +1,20 @@
 package com.zjdex.framework.util.http;
 
+import com.zjdex.framework.util.constant.ConstantUtil;
+import com.zjdex.framework.util.data.JsonUtil;
+import com.zjdex.framework.util.data.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.InetAddress;
+import java.net.URL;
 import java.net.UnknownHostException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author lindj
@@ -16,6 +24,113 @@ import java.net.UnknownHostException;
 public class HttpRequestUtil {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HttpRequestUtil.class);
+    private static final String JSON = "application/json";
+
+    private static final String FORM_URLENCODED = "application/x-www-form-urlencoded";
+
+    /**
+     * 发送HttpPost请求 json
+     *
+     * @param url    服务地址
+     * @param params String 请求参数
+     * @return json字符串
+     */
+    public static String doPost(String url, String params,
+                                Map<String, String> headers, Integer timeout) {
+        LOGGER.info("url={}", url);
+        LOGGER.info("headerParams={}", JsonUtil.objectToJson(headers));
+        LOGGER.info("bodyParams={}", JsonUtil.objectToJson(params));
+        String result = "";
+        try {
+            HttpURLConnection connection = getConnection(url, JSON, headers, timeout);
+            result = writeResponse(connection, params);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        LOGGER.info("result={}", result);
+        return result;
+    }
+
+    /**
+     * 发送HttpPost请求  x-www-form-urlencoded
+     *
+     * @param url    String
+     * @param params String
+     * @return String
+     */
+    public static String doPost(String url, String params, Integer timeout) {
+        String result = "";
+        try {
+            HttpURLConnection connection = getConnection(url, FORM_URLENCODED, null, timeout);
+            result = writeResponse(connection, params);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * 获取请求 connection
+     *
+     * @param url     请求地址
+     * @param headers 请求头
+     * @return
+     * @throws IOException
+     */
+    public static HttpURLConnection getConnection(String url, String contentType, Map<String,
+            String> headers, Integer timeout) throws IOException {
+        URL conUrl = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) conUrl.openConnection();
+        connection.setDoOutput(true);
+        connection.setDoInput(true);
+        connection.setUseCaches(false);
+        connection.setInstanceFollowRedirects(true);
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Accept", contentType);
+        connection.setRequestProperty("Content-Type", contentType);
+        connection.setConnectTimeout(timeout);
+        if (headers != null) {
+            Set<Map.Entry<String, String>> sets = headers.entrySet();
+            if (!StringUtil.isEmpty(sets)) {
+                Iterator<Map.Entry<String, String>> iterator = sets.iterator();
+                while (iterator.hasNext()) {
+                    Map.Entry<String, String> entry = iterator.next();
+                    connection.setRequestProperty(entry.getKey(), entry.getValue());
+                }
+            }
+        }
+        return connection;
+    }
+
+
+    public static String writeResponse(HttpURLConnection connection, String requestContent) throws IOException {
+        BufferedReader in = null;
+        StringBuilder result = new StringBuilder();
+
+        OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream(), "UTF-8");
+        out.append(requestContent);
+        out.flush();
+        out.close();
+        try {
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return result.toString();
+    }
 
     /**
      * 获取当前网络ip
